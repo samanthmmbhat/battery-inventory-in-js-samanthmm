159fd98de5464d3e5d4f6d2796bb215d74a8e117
samanthmm <90998580+samanthmm@users.noreply.github.com>
2023-10-26 08:06:22 +0530

    added code

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-const assert = require('assert');
-
-function countBatteriesByHealth(presentCapacities) {
-  return {
+function classifyBatteries(presentCapacities) {
+  const classifiedBatteries = {
     healthy: 0,
     exchange: 0,
-    failed: 0
+    failed: 0,
   };
+
+  for (const capacity of presentCapacities) {
+    const ratedCapacity = 120; // Rated capacity of a new battery
+    const soh = (capacity / ratedCapacity) * 100;
+
+    if (soh > 80 && soh <= 100) {
+      classifiedBatteries.healthy++;
+    } else if (soh >= 63 && soh <= 80) {
+      classifiedBatteries.exchange++;
+    } else {
+      classifiedBatteries.failed++;
+    }
+  }
+
+  return classifiedBatteries;
 }
 
-function testBucketingByHealth() {
-  console.log('Counting batteries by SoH...');
-  const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
-  assert(counts["healthy"] == 2);
-  assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
-  console.log("Done counting :)");
+// Test the function
+function testBatteryClassification() {
+  const presentCapacities = [105, 90, 60, 115, 50, 70];
+  const result = classifyBatteries(presentCapacities);
+
+  // Asserts
+  console.assert(result.healthy === 2, "Expected 2 healthy batteries");
+  console.assert(result.exchange === 2, "Expected 2 batteries for exchange");
+  console.assert(result.failed === 2, "Expected 2 failed batteries");
 }
 
-testBucketingByHealth();
+testBatteryClassification();
